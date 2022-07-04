97a904b5256da6620e973cf7e4bdee923f72aee0
Mrljsh <dragan.mrljes@vsar.edu.rs>
2022-07-04 16:08:44 +0200

    Reading from inputs, making object and pushing object into array

--- scripts.js
+++ scripts.js
@@ -8,6 +8,32 @@ function Book(title, author, numberOfPages, datePublished, read) {
     this.read = Boolean(read);
 }
 
-function addBookToLibrary () {
+function addBookToLibrary() {
     
+    const titleInput = document.querySelector('#titleInput');
+    console.log(titleInput.value);
+    const authorInput = document.querySelector('#author');
+    console.log(authorInput.value);
+    const numberOfPagesInput = document.querySelector('#noPages');
+    console.log(numberOfPagesInput.value);
+    const datePublishedInput = document.querySelector('#yearPublished');
+    console.log(datePublishedInput.value);
+    const readInput = document.querySelector('#read');
+    console.log(readInput.checked);
+
+    const inputBook = new Book(titleInput.value, authorInput.value, numberOfPagesInput.value, datePublishedInput.value, readInput.checked);
+    console.log(inputBook);
+    myBooks.push(inputBook);
+    console.log(myBooks);
+}
+
+const form = document.querySelector('#addBookForm');
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    addBookToLibrary();
+});
+
+function createBooksCard() {
+    const bookShelf = document.querySelector('.books-shelf');
+    const divCard = document.createElement('div');
 }
