55bf23e1366fbabaf3703a155c09173aff8b5bde
Mrljsh <dragan.mrljes@vsar.edu.rs>
2022-07-04 13:28:32 +0200

    Basic HTML with add book form, basic styling

---
+++ scripts.js
@@ -0,0 +1,13 @@
+let myBooks = [];
+
+function Book(title, author, numberOfPages, datePublished, read) {
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+    this.datePublished = datePublished;
+    this.read = Boolean(read);
+}
+
+function addBookToLibrary () {
+    
+}
